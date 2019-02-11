91297db6be7552e2cd407867cd9f88507bd6bb61
Eloi Ac <eac@IUT.BX1>
2019-02-11 12:11:37 +0100

    PremièreInterfaceBtnDisposal

---
+++ projet/Projet/Globalinterface.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Projet
+{
+    partial class Globalinterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Globalinterface));
+            this.Home = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Calendar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Home
+            // 
+            this.Home.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Home.BackgroundImage")));
+            this.Home.Location = new System.Drawing.Point(3, 67);
+            this.Home.Name = "Home";
+            this.Home.Size = new System.Drawing.Size(199, 141);
+            this.Home.TabIndex = 0;
+            this.Home.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.05495F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.94505F));
+            this.tableLayoutPanel1.Controls.Add(this.Home, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Calendar, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 26);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.41825F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 69.58175F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 199F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 123F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(455, 534);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // Calendar
+            // 
+            this.Calendar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Calendar.BackgroundImage")));
+            this.Calendar.Location = new System.Drawing.Point(3, 214);
+            this.Calendar.Name = "Calendar";
+            this.Calendar.Size = new System.Drawing.Size(199, 193);
+            this.Calendar.TabIndex = 8;
+            this.Calendar.UseVisualStyleBackColor = true;
+            // 
+            // Globalinterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Globalinterface";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Globalinterface";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Home;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Calendar;
+    }
+}
