7b4f8f54ce4daebd48f7a0473035330d2d5f31e2
Eloi Ac <eac@IUT.BX1>
2019-02-11 09:15:45 +0100

    AuthentificationBackV1

---
+++ projet/Projet/Authentication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet
+{
+    public partial class Authentication : Form
+    {
+        public Authentication()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DateLabel_Click(object sender, EventArgs e) => DateTime.Now.ToString("dd/mm/yy HH:mm");
+    }
+}
